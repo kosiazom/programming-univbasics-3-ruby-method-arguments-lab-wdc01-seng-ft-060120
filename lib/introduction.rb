5c34ad2017045dfb87b3f544553126fe840121af
Kosi Akporji <kosiazom@gmail.com>
2020-04-13 16:10:25 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -6,10 +6,10 @@ def introduction_with_language (name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-def introduction_with_language_optional (name, Ruby)
-  puts "Hi, my name is #{name} and I am learning to program in Ruby."
+def introduction_with_language_optional (name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
-
+introduction_with_language_optional (name, "Ruby")
 
 
 
